ba61e911d7021d3f9ef9071cb784faedb3e1eedf
Profloader <75749540+Profloader@users.noreply.github.com>
2022-01-26 22:50:13 +0100

    Add files via upload

---
+++ bondeeco_bondee.sql
@@ -0,0 +1,40 @@
+
+--
+-- Database: `bondeeco_bondee`
+--
+
+CREATE DATABASE IF NOT EXISTS `bondeeco_bondee` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `bondeeco_bondee`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pre_reg`
+--
+
+DROP TABLE IF EXISTS `pre_reg`;
+CREATE TABLE `pre_reg` (
+  `id` int(11) NOT NULL,
+  `fldName` varchar(50) NOT NULL,
+  `fldEmail` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pre_reg`
+--
+ALTER TABLE `pre_reg`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pre_reg`
+--
+ALTER TABLE `pre_reg`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
